6e97fa688f4b70944dd8bc1225b4b54ee456cdd4
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-07-07 18:19:44 -0400

    complete exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :apparel, on: :create
 
+  def apparel
+    if mens_apparel == true || womens_apparel == true
+    else
+      errors.add(:base, "Store must carry at least men's or women's apparel")
+    end
+  end
 end
