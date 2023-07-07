cfebf8cd1d2d6d855701c8f849743f108259f250
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-07-07 15:24:43 -0400

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+puts "This is #{@store1.name}"
+
+@store2 = Store.find(2)
+puts "This is #{@store2.name}"
+
+#store name updated to Montreal
+@store1.update(name: "Montreal")
+puts @store1.name
